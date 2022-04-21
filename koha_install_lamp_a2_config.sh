c687efa363e9c7b34955365efcae720ea14e4d85
Lennon Mazonde <30889263+grandmaestr@users.noreply.github.com>
2022-04-21 14:11:20 +0200

    Add line-breaks to echo commands

--- koha_install_lamp_a2_config.sh
+++ koha_install_lamp_a2_config.sh
@@ -8,40 +8,38 @@
 # You can modify or use this script however you like but I'd really appreciate it if you give me a shout out or attribution if you post it elsewhere :)
 
 # Prompt the user for the name of your koha instance. Input something simple and easily distinguishable from other instances.
-echo "Enter the name of your Koha instance:"
+echo $'\nEnter the name of your Koha instance:'
 
 # Read the input and store in a variable
 read instancename
 
 # Create the instance
 sudo koha-create --create-db $instancename
-echo "You will need to configure your instance for name-based or ip installation. See /etc/apache2/sites-available/$instancename for more." 
+echo $'\nYou will need to configure your instance for name-based or ip installation. See /etc/apache2/sites-available/$instancename for more.'
 sleep 2
 # Enable your Koha instance
 sudo a2ensite $instancename
 
 # Use sed to configure your ServerName for name-based installation.
 # Prompt user to enter their OPAC domain/subdomain
-echo
-echo "Enter the your OPAC url only (Skip http:// or https://):"
+echo $'\nEnter the your OPAC url only (Skip http:// or https://):'
 read  opacurl
-echo "Your OPAC url is: $opacurl"
+echo $'\nYour OPAC url is: $opacurl'
 
 # Replace ServerName with  OPAC url above
 sudo sed -i "/ServerName $instancename.myDNSname.org/c\   ServerName $opacurl" /etc/apache2/sites-enabled/$instancename.conf
 
 # Prompt user to enter their Staff URL
-echo
-echo "Enter the your Staff url only (Skip http:// or https://):"
+echo $'\nEnter the your Staff url only (Skip http:// or https://):'
 read  staffurl
-echo "Your Staff url is: $opacurl"
+echo $'\nYour Staff url is: $opacurl'
 
 # Replace ServerName with  OPAC url above
 sudo sed -i "/ServerName $instancename-intra.myDNSname.org/c\   ServerName $staffurl" /etc/apache2/sites-enabled/$instancename.conf
 
 
 # Check Apache for configuration errors
-echo "If you come across any errors, go to /etc/apache2/sites-enabled/$instancename."
+echo $'\nIf you come across any errors, go to /etc/apache2/sites-enabled/$instancename.'
 sleep 2
 sudo apachectl configtest
 
@@ -50,7 +48,7 @@ sudo systemctl restart apache2
 
 # Enable instance
 sudo koha-enable $instancename
-echo "The Koha instance $instancename has been successfully installed"
+echo $'\nThe Koha instance $instancename has been successfully installed'
 
 # Start Zebra
 sudo koha-zebra --restart $instancename
