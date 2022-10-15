44f61f6215140ae4ed5224e55bef6090d7447800
grandmaestr <lmmazonde@gmail.com>
2022-10-15 11:23:47 +0200

    delete obsolete scripts, update others

--- koha-mysql-init/koha-mysql-init.sh
+++ koha-mysql-init/koha-mysql-init.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-for k in  $(koha-list)  ; do koha-mysql $k < /etc/koha/koha-mysql-init.sql; done
+for k in  $(koha-list)  ; do 
+    koha-mysql $k < /etc/koha/koha-mysql-init.sql; 
+done
