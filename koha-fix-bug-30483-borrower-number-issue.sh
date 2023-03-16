94c5e721002ba5072261471e91e59570c9def937
grandmaestr <lmmazonde@gmail.com>
2023-03-16 19:20:49 +0100

    add options, multi instance processing

--- koha-fix-bug-30483-borrower-number-issue.sh
+++ koha-fix-bug-30483-borrower-number-issue.sh
@@ -4,7 +4,7 @@ set -euox pipefail
 
 usage() {
   cat <<EOF
-Usage: $(basename "$0") [koha_instance_name]
+Usage: $(basename "$0") [koha_instance_name]...
 
 This script fixes the error encountered during the Koha upgrade to version 22.06.00.041:
 Bug 30483 - Make issues.borrowernumber and issues.itemnumber NOT NULL
@@ -13,23 +13,21 @@ ERROR: {UNKNOWN}: DBI Exception: DBD::mysql::db do failed: Cannot change column 
 The script removes the foreign key constraints from the 'issues' table, updates the 'borrowernumber' and 'issues.itemnumber' columns to NOT NULL, and re-adds the foreign key constraints.
 
 Arguments:
-  koha_instance_name   The name of the Koha instance to fix the error for.
+  koha_instance_name   The name of one or more Koha instances to fix the error for.
 
 Requirements:
   - The script must be run as root or by a user with sudo privileges.
   - xmlstarlet must be installed.
 
 Example:
-  $(basename "$0") your_koha_instance_name
+  $(basename "$0") your_koha_instance_name another_koha_instance_name
 EOF
 }
 
-if [ "$#" -ne 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+if [ "$#" -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
   usage
   exit 0
 fi
-
-koha_instance_name=$1
 
 # Check if the script is being run as root or a user with sudo privileges
 if [ "$(id -u)" != "0" ]; then
@@ -43,34 +41,37 @@ if ! command -v xmlstarlet >/dev/null; then
   exit 1
 fi
 
-# Load Koha instance configuration
-kohaconfig="/etc/koha/sites/$koha_instance_name/koha-conf.xml"
-mysqlhost="$(sudo xmlstarlet sel -t -v 'yazgfs/config/hostname' "$kohaconfig")"
-mysqldb="$(sudo xmlstarlet sel -t -v 'yazgfs/config/database' "$kohaconfig")"
-mysqluser="$(sudo xmlstarlet sel -t -v 'yazgfs/config/user' "$kohaconfig")"
-mysqlpass="$(sudo xmlstarlet sel -t -v 'yazgfs/config/pass' "$kohaconfig")"
+for koha_instance_name in "$@"
+do
+  # Load Koha instance configuration
+  kohaconfig="/etc/koha/sites/$koha_instance_name/koha-conf.xml"
+  mysqlhost="$(sudo xmlstarlet sel -t -v 'yazgfs/config/hostname' "$kohaconfig")"
+  mysqldb="$(sudo xmlstarlet sel -t -v 'yazgfs/config/database' "$kohaconfig")"
+  mysqluser="$(sudo xmlstarlet sel -t -v 'yazgfs/config/user' "$kohaconfig")"
+  mysqlpass="$(sudo xmlstarlet sel -t -v 'yazgfs/config/pass' "$kohaconfig")"
 
-# Connect to MySQL database and execute the required commands
-mysql --host="$mysqlhost" --user="$mysqluser" --password="$mysqlpass" --database="$mysqldb" <<-EOF
--- Backup your database before running this script
--- Remove foreign key constraints
-ALTER TABLE issues DROP FOREIGN KEY issues_ibfk_1;
-ALTER TABLE issues DROP FOREIGN KEY issues_ibfk_2;
+  # Connect to MySQL database and execute the required commands
+  mysql --host="$mysqlhost" --user="$mysqluser" --password="$mysqlpass" --database="$mysqldb" <<-EOF
+  -- Backup your database before running this script
+  -- Remove foreign key constraints
+  ALTER TABLE issues DROP FOREIGN KEY issues_ibfk_1;
+  ALTER TABLE issues DROP FOREIGN KEY issues_ibfk_2;
 
--- Update the borrowernumber and issues.itemnumber columns to NOT NULL
-ALTER TABLE issues MODIFY borrowernumber INT NOT NULL;
-ALTER TABLE issues MODIFY itemnumber INT NOT NULL;
+  -- Update the borrowernumber and issues.itemnumber columns to NOT NULL
+  ALTER TABLE issues MODIFY borrowernumber INT NOT NULL;
+  ALTER TABLE issues MODIFY itemnumber INT NOT NULL;
 
--- Re-add the foreign key constraints
-ALTER TABLE issues
-ADD CONSTRAINT issues_ibfk_1
-FOREIGN KEY (borrowernumber) REFERENCES borrowers (borrowernumber)
-ON DELETE CASCADE ON UPDATE CASCADE;
+  -- Re-add the foreign key constraints
+  ALTER TABLE issues
+  ADD CONSTRAINT issues_ibfk_1
+  FOREIGN KEY (borrowernumber) REFERENCES borrowers (borrowernumber)
+  ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE issues
-ADD CONSTRAINT issues_ibfk_2
-FOREIGN KEY (itemnumber) REFERENCES items (itemnumber)
+  ALTER TABLE issues
+  ADD CONSTRAINT issues_ibfk_2
+  FOREIGN KEY (itemnumber) REFERENCES items (itemnumber)
 ON DELETE CASCADE ON UPDATE CASCADE;
 EOF
 
-echo "The columns borrowernumber and itemnumber have been updated to NOT NULL and foreign key constraints have been re-added."
+echo "The columns borrowernumber and itemnumber have been updated to NOT NULL and foreign key constraints have been re-added for instance $koha_instance_name."
+done
